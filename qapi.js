1dfc50562919f03e498656b783849d0508818512
Wurikos <50768680+Wurikos@users.noreply.github.com>
2020-10-21 16:36:35 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,43 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f872783791d77001ead1d36",
+				"5f872783791d77001ead1d38",
+				"5f902606283ef4001ed1351a",
+				"5f90260b283ef4001ed1351e",
+				"5f902616283ef4001ed13522"
+			]
+		},
+		"5f872783791d77001ead1d36": {
+			"id": "5f872783791d77001ead1d36",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f872783791d77001ead1d38": {
+			"id": "5f872783791d77001ead1d38",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5f902606283ef4001ed1351a": {
+			"id": "5f902606283ef4001ed1351a",
+			"pageUrl": "about",
+			"name": "about"
+		},
+		"5f90260b283ef4001ed1351e": {
+			"id": "5f90260b283ef4001ed1351e",
+			"pageUrl": "contacts",
+			"name": "contacts"
+		},
+		"5f902616283ef4001ed13522": {
+			"id": "5f902616283ef4001ed13522",
+			"pageUrl": "service",
+			"name": "service"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Em, Strong, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Section } from "@quarkly/components";
+import * as Components from "components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		---
+		<Section background="--color-primary">
+			<Components.Menumob color="#cb0d0d">
+				<Override slot="menu">
+					<Override slot="link" color="#dbe0e3" />
+				</Override>
+			</Components.Menumob>
+		</Section>
+		<Box>
+			<Box>
+				<Text>
+					<Strong>
+						<Em>
+							Some text
+						</Em>
+					</Strong>
+				</Text>
+			</Box>
+		</Box>
+	</Theme>;
+});
